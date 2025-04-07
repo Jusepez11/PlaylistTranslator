9d61f3b35ef17e71ce385f59c67e01a20e8687ff
Sebastian Lopez-Gallardo <44486060+Jusepez11@users.noreply.github.com>
2025-04-07 18:03:53 -0400

    Update songFinder.py
    now it is possible to get snippets for each item from the playlist

--- songFinder.py
+++ songFinder.py
@@ -5,13 +5,35 @@ from googleapiclient.discovery import build
 from google_auth_oauthlib.flow import InstalledAppFlow
 from urllib3 import request
 
-# Define the scopes you need
-SCOPES = ['https://www.googleapis.com/auth/youtube.force-ssl']
-flow = InstalledAppFlow.from_client_secrets_file('secrets.json', SCOPES)
-API_KEY = json.load(open('API_KEY.json'))["API_KEY"]
-youtube = build('youtube', 'v3', developerKey=API_KEY)
+# Sample Python code for youtube.playlists.list
+# See instructions for running these code samples locally:
+# https://developers.google.com/explorer-help/code-samples#python
 
-request = youtube.Search().list(
+scopes = ["https://www.googleapis.com/auth/youtube.readonly"]
 
+def main():
+    # Disable OAuthlib's HTTPS verification when running locally.
+    # *DO NOT* leave this option enabled in production.
 
-)
+    # Define the scopes you need
+    SCOPES = ['https://www.googleapis.com/auth/youtube.force-ssl']
+    #flow = InstalledAppFlow.from_client_secrets_file('secrets.json', SCOPES)
+    API_KEY = json.load(open('API_KEY.json'))["API_KEY"]
+    youtube = build('youtube', 'v3', developerKey=API_KEY)
+
+    request = youtube.playlistItems().list(
+        part="snippet,contentDetails",
+        playlistId="PLrCCqE1vWhlxeIdKkB-J5LXJZiVpdyFiw",
+        maxResults=50
+    )
+    response = request.execute()
+
+# For testing to check that we are getting what we need, title and link
+    #TODO get artist name 
+    for item in response["items"]:
+        title = item["snippet"]["title"]
+        video_id = item["snippet"]["resourceId"]["videoId"]
+        print(f"{title} - https://www.youtube.com/watch?v={video_id}")
+
+if __name__ == "__main__":
+    main()
